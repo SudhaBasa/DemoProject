b8d09e7de4085b391e1edd37b83a91e442e4aa99
sudha <sudhabasa8@gmail.com>
2024-11-08 20:19:12 -0600

    commiting updated files

--- src/test/java/dsalgo_TestRunner/TestNgRunner.java
+++ src/test/java/dsalgo_TestRunner/TestNgRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-features={"src/test/resources/Features"},
+features={"src/test/resources/Features/QueuePage.feature"},
 //features={"src/test/resources/Features/RegisterPage.feature", "src/test/resources/Features/QueuePage.feature"},  //to run two different feature files
 //features= {"@target/rerun.txt"},
 
@@ -14,9 +14,9 @@ features={"src/test/resources/Features"},
 publish=true,
 glue= {"dsalgo_StepDefinition","dsalgo_Hooks"},
 plugin={"pretty","html:target/CucumberReports/CucumberReport.html",
-		"json:target/report.json",
+		
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+		}
 //dryRun=true, // checks mapping between scenario steps and step definition methods
 		
 		)
--- src/test/java/dsalgo_TestRunner/TestRunner.java
+++ src/test/java/dsalgo_TestRunner/TestRunner.java
@@ -1,28 +1,28 @@
-//package dsalgo_TestRunner;
-//
-//import org.junit.runner.RunWith;
-//
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//features={"src/test/resources/Features/RegisterPage.feature"},
-////features={"src/test/resources/Features/RegisterPage.feature", "src/test/resources/Features/QueuePage.feature"},  //to run two different feature files
-////features= {"@target/rerun.txt"},
-//
-//tags = "@TS_Register_06",
-//publish=true,
-//glue= {"dsalgo_StepDefinition","dsalgo_Hooks"},
-//plugin={"pretty","html:target/CucumberReports/CucumberReport.html",
-//		"json:target/report.json",
-//		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-//		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
-////dryRun=true, // checks mapping between scenario steps and step definition methods
-//		
-//		)
-//
-//public class TestRunner {
-//	
-//
-//}
+package dsalgo_TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features={"src/test/resources/Features/Queue.feature"},
+//features={"src/test/resources/Features/RegisterPage.feature", "src/test/resources/Features/QueuePage.feature"},  //to run two different feature files
+//features= {"@target/rerun.txt"},
+
+tags = "@TS_Queue_04",
+publish=true,
+glue= {"dsalgo_StepDefinition","dsalgo_Hooks"},
+plugin={"pretty","html:target/CucumberReports/CucumberReport.html",
+		
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		}
+//dryRun=true, // checks mapping between scenario steps and step definition methods
+		
+		)
+
+public class TestRunner {
+	
+
+}
